e0ae661c3a33675e99b76e753ed6a82c076cedad
RamiRBY <ramichatgptby@gmail.com>
2025-08-11 20:18:14 +0200

    first commit

---
+++ src/main/java/com/rby/demo/sprinai_demo/SprinaiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.rby.demo.sprinai_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinaiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinaiDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rby/demo/sprinai_demo/SprinaiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rby.demo.sprinai_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinaiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
